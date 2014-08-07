fcb081fcade4ab134c9289f222d29877795e8973
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2014-08-07 19:16:48 +0200

    scisoft

--- setup_annarella.sh
+++ setup_annarella.sh
@@ -16,6 +16,9 @@ sudo aptitude install b43-fwcutter firmware-b43-installer
 # Remove ATI fglrx driver
 sudo apt-get remove fglrx fglrx-amdcccle --purge
 
+### 3D and OpenGL
+sudo apt-get install mesa-utils
+
 ### Revision control (Git & friends)
 sudo apt-get install git meld gftp
 #subversion
@@ -26,8 +29,11 @@ sudo dpkg -i dropbox_1.6.0_amd64.deb
 #$ dropbox start -i
 
 ### Python
-sudo apt-get install python-pip python-setuptools
-sudo aptitude install python-matplotlib python-scipy python-sphinx python-lxml python-sqlalchemy python-docutils python-h5py python-sympy python-pandas python-openbabel
+sudo aptitude install python-pip python-setuptools
+sudo aptitude install python-matplotlib python-scipy python-sphinx python-docutils python-h5py
+sudo aptitude install python-sympy python-pandas python-openbabel python-sqlalchemy python-lxml
+# Python Visual (NOT WORKING YET!!!)
+sudo aptitude install python-visual libgtkglextmm-x11-1.2-dev
 
 ### Python modules via PIP (NOTE: better from user)
 #### ipython upgrade
@@ -36,12 +42,20 @@ sudo pip install ipython -U
 sudo easy_install -U distribute
 sudo pip install matplotlib -U
 
+#### XKCD plots in matplotlib (required version >1.3)
+sudo apt-get install gnome-font-viewer
+cd ~/utils
+wget http://antiyawn.com/uploads/Humor-Sans.ttf
+sudo gnome-font-viewer Humor-Sans.ttf
+# (click on install button, that's all for the fonts!)
+# in matplotlib, any plot can be converted to xkcd via -> plt.xkcd()
+
 ### Editor (Emacs & friends)
 sudo add-apt-repository ppa:cassou/emacs
 sudo apt-get install emacs24 emacs24-el emacs24-common-non-dfsg
 sudo aptitude install fonts-inconsolata aspell-en aspell-fr aspell-it
 
-### Office (Libreoffice & C)
+### Office (Libreoffice & friends)
 sudo apt-get remove abiword gnumeric --purge
 sudo add-apt-repository ppa:libreoffice/libreoffice-4-2
 sudo aptitude install libreoffice
@@ -66,13 +80,9 @@ mkdir .local/share/data/Mendeley\ Ltd.
 ln -s MendeleyDB .local/share/data/Mendeley\ Ltd./Mendeley\ Desktop
 # assuming your Mendely configuration file is utils/Mendeley\ Desktop.conf
 mkdir .config/Mendeley\ Ltd.
-ln -s utils/Mendeley\ Desktop.conf .config/Mendeley\ Ltd./Mendeley\ Desktop.conf
-
-### 3D and OpenGL
-sudo apt-get install mesa-utils
-
-### Python Visual
-sudo aptitude install python-visual libgtkglextmm-x11-1.2-dev
+#ln -s utils/Mendeley\ Desktop.conf .config/Mendeley\ Ltd./Mendeley\ Desktop.conf
+# Mendely does not like to work with a link, for some strange reason one has to copy the configuration file!
+cp utils/Mendeley\ Desktop.conf .config/Mendeley\ Ltd./Mendeley\ Desktop.conf
 
 ### Web
 # default flash plugin does not always work properly... Adobe one is better
@@ -92,15 +102,6 @@ sudo aptitude install claws-mail claws-mail-extra-plugins
 
 
 ### TODO ###
-
-### XKCD plots in matplotlib (required version >1.3)
-sudo apt-get install gnome-font-viewer
-cd ~/utils
-wget http://antiyawn.com/uploads/Humor-Sans.ttf
-sudo gnome-font-viewer Humor-Sans.ttf
-# (click on install button, that's all for the fonts!)
-# in matplotlib, any plot can be converted to xkcd via -> plt.xkcd()
-
 
 ### Utils & generic
 sudo echo "deb http://download.virtualbox.org/virtualbox/debian precise contrib" >> /etc/apt/sources.list
@@ -138,7 +139,7 @@ sudo chown mauro /usr/local/maulocal
 cd; ln -s /usr/local/maulocal local
 
 ### LaTeX (Texlive 2014)
-#https://github.com/scottkosty/install-tl-ubuntu
+# https://github.com/scottkosty/install-tl-ubuntu
 cd local; git clone https://github.com/scottkosty/install-tl-ubuntu.git
 cd install-tl-ubuntu; sudo ./install-tl-ubuntu
 # restart computer
@@ -148,7 +149,62 @@ cd ~/local/
 sudo aptitude install python-qt4 python-qt4-dev
 git clone https://github.com/vasole/pymca.git
 cd pymca
+#sudo SPECFILE_USE_GNU_SOURCE=1 python setup.py install
+#
+# make USER INSTALL (TODO: not yet working!!!)
+#SPECFILE_USE_GNU_SOURCE=1 python setup.py build
+# use sys.path.append('/home/mauro/local/pymca/build/lib.linux-x86_64-2.7/PyMca5/') in scripts
+#
+# make CLEAN:
+#(sudo) rm -rf /usr/local/lib/python2.7/dist-packages/PyMca*
+#(sudo) rm -rf ~/local/pymca/build/
+
+### PyMca 4.7
+cd ~/local/
+sudo aptitude install libqwt5-qt4 libqwt-dev python-qwt5-qt4
+svn checkout http://svn.code.sf.net/p/pymca/code/ pymca-code
+cd pymca-code
 sudo SPECFILE_USE_GNU_SOURCE=1 python setup.py install
+
+
+### XRT
+# http://pythonhosted.org/xrt/
+cd ~/local/
+wget https://pypi.python.org/packages/source/x/xrt/xrt-0.9.4.zip#md5=53f89fe6d94b59a66bb4a66a6326f95e
+unzip xrt-0.9.4.zip
+# use sys.path.append('~/local/xrt-0.9.4/xrt') in your scripts
+
+### XOP/SHADOW3
+export MYLOCAL=~/local/
+cd $MYLOCAL
+wget http://ftp.esrf.eu/pub/scisoft/xop2.3/xop2.3_Linux_20140616.tar.gz
+tar xzvf xop2.3_Linux_20140616.tar.gz
+export XOP_HOME=$MYLOCAL/xop2.3
+cd $MYLOCAL
+mkdir xop_extensions
+cd xop_extensions
+wget http://ftp.esrf.eu/pub/scisoft/xop2.3/shadowvui1.12_Linux_20140708.tar.gz
+tar xzvf shadowvui1.12_Linux_20140708.tar.gz
+cd $MYLOCAL/xop2.3/extensions
+ln -s $MYLOCAL/xop_extensions/shadowvui shadowvui
+# IF YOU WANT TO UPDATE SHADOW3 TO THE LAST VERSION
+# cd shadow3
+# git pull
+# OR if this does not work:
+#    cd ..; rm -rf shadow3;
+#    git clone git://git.epn-campus.eu/repositories/shadow3
+#    cd shadow3
+# make
+# make python
+export SHADOW3_HOME=$MYLOCAL/xop_extensions/shadowvui/shadow3
+export SHADOW3_BUILD=$SHADOW3_HOME/build/lib.linux-x86_64-2.7
+export LD_LIBRARY_PATH=$SHADOW3_HOME:$LD_LIBRARY_PATH
+export PYTHONPATH=$SHADOW3_BUILD:$PYTHONPATH
+
+# TIPS:
+# run shadow with 'xop shadowvui'
+# put all previous environment variables in .bashrc
+sudo ln -s $MYLOCAL/xop2.3/xop /usr/local/bin/xop
 
 ### Emacs-related (emacs24 installed system-wide via Damien Cassou PPA)
 cd; ln -s utils/software-notes/mydotemacs24U1204 .emacs
@@ -173,13 +229,6 @@ cd ~/local/crackrar-0.2
 sudo aptitude install libxml2-dev
 make
 sudo make install
-
-### PyMca
-cd ~/local/
-sudo aptitude install python-qt4 python-qt4-dev libqwt5-qt4 libqwt-dev python-qwt5-qt4
-svn checkout http://svn.code.sf.net/p/pymca/code/ pymca-code
-cd pymca-code
-sudo SPECFILE_USE_GNU_SOURCE=1 python setup.py install
 
 ### Larch 
 #Instructions: http://xraypy.github.com/xraylarch/downloads/index.html#source-installation
@@ -222,4 +271,5 @@ ln -s /home/mauro/WORK11 /media/sf_WinLinShare/WORK11
 
 ## BASH VARIABLES
 ## ==============
-export PYTHONPATH=$HOME/utils:$PYTHONPATH
+## see: mydotbashrcU1204
+
