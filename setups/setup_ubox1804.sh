7162e4763ae0a8cc7d46a851bbc35724c47a92df
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2019-03-21 05:46:56 +0100

    atom packages

--- setups/setup_ubox1804.sh
+++ setups/setup_ubox1804.sh
@@ -82,6 +82,9 @@ wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt update
 sudo apt install atom
+#installed packages
+apm install language-restructuredtext language-latex
+
 #EMACS
 sudo add-apt-repository ppa:ubuntu-elisp/ppa
 sudo apt update
