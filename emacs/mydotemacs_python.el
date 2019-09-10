92ef02d5c784923fa15c10ba2f669f4bb493e135
Mauro Rovezzi <mauro.rovezzi@gmail.com>
2019-09-10 09:46:32 +0200

    yaml fix

--- emacs/mydotemacs_python.el
+++ emacs/mydotemacs_python.el
@@ -63,4 +63,18 @@
 (global-set-key (kbd "C-x g") 'magit-status)
 
 ;; YAML
+(require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(yaml-mode py-autopep8 material-theme magit flycheck elpy ein better-defaults)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
